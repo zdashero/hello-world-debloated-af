97454af6426c53da80de3120ce7a898c01092eb6
zdashero <166456558+zdashero@users.noreply.github.com>
2024-06-18 00:02:54 +0100

    even further beyond
    not a single byte wasted

--- hello.asm
+++ hello.asm
@@ -1,16 +1,13 @@
-.global _start
+.global q
 .intel_syntax noprefix
-
-_start:
+q:
     mov rax, 1
     mov rdi, 1
-    mov rsi, hello_world
+    mov rsi, a
     mov rdx, 12
     syscall
-
     mov rax, 60
     xor rdi, rdi
     syscall
-
-hello_world:
-    .asciz "hewwo wowwd\n"
+a:
+    .byte 'h', 'e', 'w', 'w', 'o', ' ', 'w', 'o', 'w', 'w', 'd', '\n', 0

a03cfe9cd859613c7b6fe05686906869aaa9c76c
zdashero <166456558+zdashero@users.noreply.github.com>
2024-06-17 23:54:37 +0100

    debloated even more
    Used mov rsi, hello_world instead of lea rsi, [hello_world]: Both achieve the same result, but mov is more straightforward here 
    
    Used xor rdi, rdi to set rdi to 0: This is more concise than mov rdi, 0.

--- hello.asm
+++ hello.asm
@@ -1,17 +1,15 @@
 .global _start
 .intel_syntax noprefix
 
-// Debloated as fuck! ~ Goldy
-
 _start:
     mov rax, 1
     mov rdi, 1
-    lea rsi, [hello_world]
+    mov rsi, hello_world
     mov rdx, 12
     syscall
 
     mov rax, 60
-    mov rdi, 0
+    xor rdi, rdi
     syscall
 
 hello_world:
